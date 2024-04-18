533a251bf94c2c555b036299bdb316f1a1d17255
Julie Wustefeld <Urubaean>
2024-04-18 16:58:24 +0200

    Create structure and start on loop

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+//variables
+    const main = document.querySelector('main');
+    const display = document.querySelector('.display');
+    const book = document.querySelector(".book");
+    const cover = document.querySelector(".bookcover");
+    const blocktxt = document.querySelector(".blocktxt");
+    const title = document.querySelector(".booktitle");
+    const author = document.querySelector(".bookauthor")
+    const synopsis = document.querySelector(".booksynopsis");
+    const tags = document.querySelector(".booktags");
+    const singletag = document.querySelector(".tagname");
+    const bookinfo = document.querySelector(".bookinfo");
+    const release = document.querySelector(".bookrelease");
+    const link = document.querySelector(".booklink");
+    const logo = document.querySelector(".goodreadslogo");
+    const publisher = document.querySelector(".bookpublisher");
+
+    function createDiv(type,parent,content,className) {
+        const newDiv=document.createElement(type);
+        if (content!=null) {
+        newDiv.innerHTML=content;
+        }
+        if (className!=null) {
+        newDiv.classList.add(className);
+        }
+        parent.appendChild(newDiv);
+        return newDiv;
+        }
+
+    const displayer = createDiv("section", main, "", "display");
+
+    for (let i=0; i<library.length; i++) {
+        const container = createDiv("article", displayer, "", "book");
+    }
+}
+)
+
