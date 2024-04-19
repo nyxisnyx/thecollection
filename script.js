199f0dc917a9ce488a008e4a12a683da13097868
JulieWustefeld <juliewustefeld@gmail.com>
2024-04-19 10:40:04 +0200

    Add JSon attributes to the for loop

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+fetch('library.json').then((response) => response.json()).then((json) => {
+    console.log(json)
+
 //variables
     const main = document.querySelector('main');
-    const display = document.querySelector('.display');
-    const book = document.querySelector(".book");
-    const cover = document.querySelector(".bookcover");
-    const blocktxt = document.querySelector(".blocktxt");
-    const title = document.querySelector(".booktitle");
-    const author = document.querySelector(".bookauthor")
-    const synopsis = document.querySelector(".booksynopsis");
-    const tags = document.querySelector(".booktags");
-    const singletag = document.querySelector(".tagname");
-    const bookinfo = document.querySelector(".bookinfo");
-    const release = document.querySelector(".bookrelease");
-    const link = document.querySelector(".booklink");
-    const logo = document.querySelector(".goodreadslogo");
-    const publisher = document.querySelector(".bookpublisher");
 
     function createDiv(type,parent,content,className) {
         const newDiv=document.createElement(type);
@@ -33,18 +21,27 @@ document.addEventListener("DOMContentLoaded", (event) => {
     for (let i=0; i<library.length; i++) {
         const container = createDiv("article", displayer, "", "book");
         const bookcover = createDiv('img', container, '', '.bookcover');
+            bookcover.setAttribute('src', library.cover);
+            bookcover.setAttribute('alt', library.alt)
         const txtblock = createDiv('div', container, '', 'blocktxt');
         const booktitle = createDiv('h2', txtblock, '', 'booktitle');
+            booktitle.innerHTML = library[i].title;
         const bookauthor = createDiv('h3', txtblock, '', 'bookauthor');
+            bookauthor.innerHTML = library[i].author;
         const booksynopsis = createDiv('p', txtblock, '', 'booksynopsis');
+            booksynopsis.innerHTML = library[i].synopsis;
         const booktags = createDiv('p', txtblock, '', 'booktags');
+            booktags.innerHTML = library[i].tags
         const tagname = createDiv('p', booktags, '', 'tagname');
         const bookinfo = createDiv('div', txtblock, '', 'bookinfo');
         const bookrelease = createDiv('p', bookinfo, '', 'bookrelease');
+            bookrelease.innerHTML = library[i].release;
         const booklink = createDiv('a', bookinfo, '', 'booklink');
+            booklink.setAttribute('src', library[i].goodreads);
         const goodreadslogo = createDiv('img', booklink, '', 'goodreadslogo');
+            goodreadslogo.setAttribute('src', 'assets/goodreadslogo.png');
+            goodreadslogo.setAttribute('alt', 'the goodreads logo');
         const bookpublisher = createDiv('p', bookinfo, '', 'bookpublisher');
+            bookpublisher.innerHTML = library[i].publisher;
     }
-}
-)
-
+})
