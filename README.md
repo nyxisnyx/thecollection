6cb16b523b6751d06404a6cc2d2fe141b61c45d7
JulieWustefeld <juliewustefeld@gmail.com>
2024-04-19 15:32:35 +0200

    Add SASS

---
+++ README.md
@@ -0,0 +1,3 @@
+Background image by Pavel Danilyuk: https://www.pexels.com/fr-fr/photo/bleu-peinture-abstrait-colore-8381923/
+
+Reset by Josh W. Comeau: https://www.joshwcomeau.com/css/custom-css-reset/
--- script.js
+++ script.js
@@ -21,7 +21,9 @@ fetch('library.json').then((response) => response.json()).then((library) => {
     for (let i=0; i<library.length; i++) {
         const container = createDiv("article", displayer, "", "book");
 
-        const bookcover = createDiv('img', container, '', '.bookcover');
+        const sectioncover = createDiv('div', container, '', 'sectioncover');
+
+        const bookcover = createDiv('img', sectioncover, '', 'bookcover');
             bookcover.setAttribute('src', library[i].cover);
             bookcover.setAttribute('alt', library[i].alt);
 
